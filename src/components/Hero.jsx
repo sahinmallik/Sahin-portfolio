bbb2fa28d5ccb780fbdde14e4f55fa6ce76f3797
SAHIN MALLIK <86676300+sahinmallik@users.noreply.github.com>
2024-07-04 21:10:46 +0530

    resume link is added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -50,7 +50,7 @@ const Hero = () => {
               transition={{ delay: 1.5, duration: 0.5 }}
               type="button"
               class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 cursor-pointer"
-              href="#"
+              href="https://drive.google.com/file/d/1jRXUhIyMxVF7Mac2biUWIG5U_nbEbjlI/view?usp=share_link"
             >
               Get My Resume
             </motion.a>
